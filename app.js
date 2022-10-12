26214f844f13a474d19b8a4dd0a6922eda142f91
SkylynxX <meshcheriakova.elizaveta@gmail.com>
2022-10-13 01:23:25 +0300

    fix error

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
+const app = express();
 // Apply the rate limiting middleware to all requests
 app.use(limiter)
-const app = express();
 app.use(helmet());
 mongoose.connect('mongodb://localhost:27017/moviedb', {
   useNewUrlParser: true,
